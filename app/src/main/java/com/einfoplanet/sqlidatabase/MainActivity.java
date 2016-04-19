144c81221753ffbc908c6fa060f6039ab0f0c913
eshvar289 <eshvar89@hotmail.com>
2016-04-19 10:34:54 +0530

    Adding lines of code for the insertion of contact.

--- app/src/main/java/com/einfoplanet/sqlidatabase/MainActivity.java
+++ app/src/main/java/com/einfoplanet/sqlidatabase/MainActivity.java
@@ -9,5 +9,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DBAdapter dbAdapter=new DBAdapter(this);
+        //adding a contact
+        dbAdapter.open();
+        long id= dbAdapter.insertContact("Eshvar Mali","eshvar89@hotmail.com");
+             id= dbAdapter.insertContact("Dinkar Mali","dinkarraj@gmail.com");
+
+        dbAdapter.close();
     }
 }

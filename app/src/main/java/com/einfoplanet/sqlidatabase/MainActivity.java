4ce2e76e8e5041d86ab4b1a84eadaa83fb91adaf
eshvar289 <eshvar89@hotmail.com>
2016-04-19 11:31:05 +0530

    Code for retrieving the contact from the database.

--- app/src/main/java/com/einfoplanet/sqlidatabase/MainActivity.java
+++ app/src/main/java/com/einfoplanet/sqlidatabase/MainActivity.java
@@ -1,31 +1,42 @@
 package com.einfoplanet.sqlidatabase;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    //text view for displaying the contact
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*
-        * HOW IT WORKS
-        * in this example ,we first created an instance of DBAdapter class.
-        *       DBAdapter db=new DBAdapter(this);
-        *    The insertContact() method returns the ID of the inserted row.If an error occure during the
-         *    operation , it returns -1
-         *
-         *    If we examint the file system of android device/emulator using DDMS,we can see the MyDB database is
-         *    created under the database folder.
-        * */
+        //instantiate the textview
+        textView= (TextView) findViewById(R.id.textView);
+        textView.setVisibility(View.GONE);
+        //creating an instance of DBAdapter
         DBAdapter dbAdapter=new DBAdapter(this);
-        //adding a contact
-        dbAdapter.open();
-        long id= dbAdapter.insertContact("Eshvar Mali","eshvar89@hotmail.com");
-             id= dbAdapter.insertContact("Dinkar Mali","dinkarraj@gmail.com");
 
+        //get a contact
+        dbAdapter.open();
+        Cursor cursor=dbAdapter.getContact(2);
+        if (cursor.moveToFirst()){
+            displayContact(cursor);
+        }
+        else Toast.makeText(this,"NO CONTACT FOUND",Toast.LENGTH_LONG).show();
         dbAdapter.close();
+    }
+
+    private String displayContact(Cursor cursor) {
+        String str;
+         str=cursor.getString(1)+"\n"+cursor.getString(2);
+
+        textView.setText(str);
+        textView.setVisibility(View.VISIBLE);
+        return  str;
     }
 }

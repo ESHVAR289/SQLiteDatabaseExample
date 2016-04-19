5e0ba8bc99bae9c205f64e313f9f35966f0d17db
eshvar289 <eshvar89@hotmail.com>
2016-04-19 10:40:34 +0530

    Description about working of the insertion code.

--- app/src/main/java/com/einfoplanet/sqlidatabase/MainActivity.java
+++ app/src/main/java/com/einfoplanet/sqlidatabase/MainActivity.java
@@ -10,6 +10,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
+        * HOW IT WORKS
+        * in this example ,we first created an instance of DBAdapter class.
+        *       DBAdapter db=new DBAdapter(this);
+        *    The insertContact() method returns the ID of the inserted row.If an error occure during the
+         *    operation , it returns -1
+         *
+         *    If we examint the file system of android device/emulator using DDMS,we can see the MyDB database is
+         *    created under the database folder.
+        * */
         DBAdapter dbAdapter=new DBAdapter(this);
         //adding a contact
         dbAdapter.open();

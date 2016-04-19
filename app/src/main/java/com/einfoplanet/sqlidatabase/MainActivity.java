1d1b94424bd20676991984bf8dfd9d85b1d44536
eshvar289 <eshvar89@hotmail.com>
2016-04-19 12:26:56 +0530

    Code for the updating the contact.

--- app/src/main/java/com/einfoplanet/sqlidatabase/MainActivity.java
+++ app/src/main/java/com/einfoplanet/sqlidatabase/MainActivity.java
@@ -2,6 +2,7 @@ package com.einfoplanet.sqlidatabase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //instance for the DBAdapter class
+        DBAdapter dbAdapter=new DBAdapter(this);
+
+        dbAdapter.open();
+        if (dbAdapter.updateContact(1,"Eshvar Mali","eshvar89@hotmail.com")){
+            Toast.makeText(this,"Update Successfully.",Toast.LENGTH_LONG).show();
+        }else
+            Toast.makeText(this,"Update failed",Toast.LENGTH_LONG).show();
+
+        dbAdapter.close();
     }
 }
